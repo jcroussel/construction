712dc115cad37ee187a22fb9a469fce950ec859f
Brunnhilde Dayez <jcbubu59@gmail.com>
2016-05-04 20:04:51 +0200

    integration bubu

--- components/footer.php
+++ components/footer.php
@@ -6,7 +6,7 @@
             <li><a href="/contact.html">Contact</a></li>
             <li><a href="/newsletter.html">Newsletter</a></li>
             <li><a href="/form.html">Questionnaire</a></li>
-            <li class="footer-social"><a href="https://www.facebook.com/Bubu-D&#xe9;co-630107567136751/" title=""><i class="fa fa-facebook-official" aria-hidden="true"></i> <span>Facebook</span></a></li>
+            <li class="footer-social"><a target="_blank" href="https://www.facebook.com/Bubu-D&#xe9;co-630107567136751/" title=""><i class="fa fa-facebook-official" aria-hidden="true"></i> <span>Facebook</span></a></li>
         </ul>
         <p class="copyright">
             &copy; Bubu Déco 2016
